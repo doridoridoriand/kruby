5d0af613c76320e9ccc9d063d0036f56ba51b155
Dorian (Takahiro Ishida) <i.am.eager.for.peace@gmail.com>
2026-03-06 18:46:49 +0900

    test(e2e): add changed-mode mapping spec (T017)

---
+++ kubernetes/spec/e2e/changed_mode_selection_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "changed mode selector resolution" do
+  it "maps core_v1_api changes to core/v1 pod selectors" do
+    context = SpecSupport::E2E::RunContext.from_env(
+      "E2E_MODE" => "changed",
+      "BASE_REF" => "origin/HEAD",
+      "E2E_FALLBACK_STRATEGY" => "minimal-smoke"
+    ).with_changed_files(["kubernetes/lib/kubernetes/api/core_v1_api.rb"])
+
+    dispatcher = SpecSupport::E2E::ModeDispatcher.new
+    selection = dispatcher.dispatch(context)
+
+    expect(selection.mode).to eq("changed")
+    expect(selection.fallback_used).to be(false)
+    expect(selection.resolved_targets).to include("core/v1/pods:create")
+    expect(selection.resolved_targets).to include("core/v1/pods:delete")
+  end
+end
